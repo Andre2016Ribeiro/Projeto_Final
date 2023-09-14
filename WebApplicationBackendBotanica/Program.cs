074971977ac0c9b3fa72bc852b498b9eef7bcd8f
Andre.Ribeiro <Andre.Ribeiro@PC024>
2023-09-14 13:16:33 +0100

    views v02

--- WebApplicationBackendBotanica/Program.cs
+++ WebApplicationBackendBotanica/Program.cs
@@ -1,7 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using WebApplicationBackendBotanica.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<WebApplicationBackendBotanicaContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApplicationBackendBotanicaContext"));
+
+});
+
 
 var app = builder.Build();
 
@@ -11,6 +33,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
+app.UseHttpsRedirection();
 
 app.UseRouting();
 
@@ -21,3 +44,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
