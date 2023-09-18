2a3bb8efb278a39fa19116a298c48d4aaa4b0b6b
John Doe <johndoe@example.com>
2023-09-18 19:03:29 +0100

    vs3

--- BotanicaEncomendasEnd/obj/Release/net7.0/BotanicaEncomendasEnd.AssemblyInfo.cs
+++ BotanicaEncomendasEnd/obj/Release/net7.0/BotanicaEncomendasEnd.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-BotanicaEncomendasEnd-5ed754dc-3472-44af-9ec4-b44144398046")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BotanicaEncomendasEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
